c5b2e00ddefb3a1d4da3f674673615eafc50c256
Lalise <lalisefufi5@gmail.com>
2025-01-17 16:48:37 +0300

    Add files via upload

---
+++ mms/BusinessLayer/Interface/IMstUserService.cs
@@ -0,0 +1,20 @@
+﻿
+using SharedLayer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+
+    public interface IMstUserService
+    {
+        Task<IEnumerable<MstUser>> GetAllUsersAsync();
+        Task<MstUser> GetUserByIdAsync(int userId);       
+        Task AddUserAsync(MstUser user);
+        Task UpdateUserAsync(MstUser user);
+        Task DeleteUserAsync(int userId);
+        Task<bool> UserNameExistsAsync(string userName);
+        Task<bool> EmailAddressExistsAsync(string emailAddress);
+        Task<bool> EmployeeIdExistsAsync(int empId);
+    }
+}
