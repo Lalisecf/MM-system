1cfe2dfd47932a321052dfc0faaf841affc32fdd
Lalise <lalisefufi5@gmail.com>
2025-01-17 16:45:53 +0300

    Add files via upload

---
+++ mms/BusinessLayer/Interface/IMstMenuDefWebService.cs
@@ -0,0 +1,17 @@
+﻿using SharedLayer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IMstMenuDefWebService
+    {
+        Task<IEnumerable<MstMenuDefWeb>> GetAllMenusAsync();
+        Task<MstMenuDefWeb> GetMenuByIdAsync(int menuCode);
+        Task AddMenuAsync(MstMenuDefWeb menu);
+        Task UpdateMenuAsync(MstMenuDefWeb menu);
+        Task DeleteMenuAsync(int menuCode);
+        Task<bool> MenuCodeExistsAsync(int menuCode);
+        Task<IEnumerable<MstMenuDefWeb>> GetSubMenusAsync(int parentCode);
+    }
+}
