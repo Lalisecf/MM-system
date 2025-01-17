809eb9452550d93b46693a916264fcb616e98a3b
Lalise <lalisefufi5@gmail.com>
2025-01-17 16:48:09 +0300

    Add files via upload

---
+++ mms/BusinessLayer/Interface/IMstUserRoleService.cs
@@ -0,0 +1,15 @@
+﻿using SharedLayer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services
+{
+    public interface IMstUserRoleService
+    {
+        Task<IEnumerable<MstUserRole>> GetAllUserRolesAsync();
+        Task<MstUserRole> GetUserRoleAsync(int userId, int roleId);
+        Task AddUserRoleAsync(MstUserRole userRole);
+        Task DeleteUserRoleAsync(int userId, int roleId);
+        Task<bool> UserRoleExistsAsync(int userId, int roleId);
+    }
+}
