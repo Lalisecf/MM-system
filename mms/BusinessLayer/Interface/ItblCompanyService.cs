2048f53a3cdf1b03b4a5c692496db2884a0890f9
Lalise <lalisefufi5@gmail.com>
2025-01-17 16:48:58 +0300

    Add files via upload

---
+++ mms/BusinessLayer/Interface/ItblCompanyService.cs
@@ -0,0 +1,13 @@
+﻿using SharedLayer.Models.Inventory;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public interface ItblCompanyService
+    {
+        Task<IEnumerable<tblCompany>> GetTblCompanies();
+        Task Update(tblCompany company);
+        Task<tblCompany> tblcompanyByName(string menuCode);
+    }
+}
