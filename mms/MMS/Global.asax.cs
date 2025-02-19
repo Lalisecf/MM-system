33ce5f8e37db913d682f53d3ba9ae2786149f3b2
Lalise <lalisefufi5@gmail.com>
2025-02-19 12:11:27 +0300

    Add files via upload

---
+++ mms/MMS/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Unity;
+
+namespace MMS
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            var container = new UnityContainer();
+            container.AddExtension(new Diagnostic());
+            log4net.Config.XmlConfigurator.Configure();
+        }
+    }
+}
---
+++ mms/MMS/Template.Master.cs
@@ -0,0 +1,24 @@
+﻿using SharedLayer.Models;
+using System;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+
+namespace Master
+{
+    public partial class Template : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var user = Session["UserName"]?.ToString();
+            var userid = Session["UserId"]?.ToString();
+            var branchName = Session["BranchName"]?.ToString();
+            var roleName = Session["RoleName"]?.ToString();
+            lblTitle.Text = Session["MenuTitle"] == null ? "" : Session["MenuTitle"].ToString();
+            if (user == null || userid == null || branchName == null || roleName == null)
+            {
+                Response.Redirect("~/Login/Login");
+            }
+        }
+       
+    }
+}
